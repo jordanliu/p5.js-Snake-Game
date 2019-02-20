813a0423ce4c950f35e51371d920ae03690a33e1
Jordan Liu <jordanxavierliu@gmail.com>
2019-02-20 15:44:47 -0500

    Snake() created

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+ createCanvas(600, 600);
+
+}
+
+function draw() {
+  background(51);
+}
+
+function Snake(){
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 0;
+  this.yspeed=0;
+}
